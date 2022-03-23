8d866c00b6bfcfd3924322ea3266897c3d1b9382
Georgi <georgi5481@abv.bg>
2022-03-23 20:14:38 +0200

    Engine config

---
+++ Engine/config/EngineConfig.h
@@ -0,0 +1,24 @@
+#ifndef ENGINE_CONFIG_ENGINECONFIG_H_
+#define ENGINE_CONFIG_ENGINECONFIG_H_
+
+//C system includes
+
+//C++ system includes
+#include<cstdint>
+
+//3rd-party includes
+
+//Own includes
+#include "sdl_utils/MonitorWindow.h"
+
+//Forward Declaration
+
+struct EngineConfig{
+	MonitorWindow windowCfg;
+	//soon to populate other configs
+
+};
+
+
+
+#endif /* ENGINE_CONFIG_ENGINECONFIG_H_ */
