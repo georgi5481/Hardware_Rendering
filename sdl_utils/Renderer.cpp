58b713130f5dd5ea39d48df40cb6a569da1de10b
Georgi <georgi5481@abv.bg>
2022-04-09 13:30:54 +0300

    adding renderer class for double layering

---
+++ sdl_utils/Renderer.cpp
@@ -0,0 +1,21 @@
+//Corresponding header
+#include "Renderer.h"
+
+//C system includes
+
+//C++ system includes
+
+//3rd-party includes
+
+//Own includes
+
+
+Renderer::Renderer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Renderer::~Renderer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ sdl_utils/Renderer.h
@@ -0,0 +1,21 @@
+#ifndef SDL_UTILS_RENDERER_H_
+#define SDL_UTILS_RENDERER_H_
+
+//C system includes
+
+//C++ system includes
+#include<cstdint>
+
+//3rd-party includes
+
+//Own includes
+
+//Forward Declaration
+
+class Renderer {
+public:
+	Renderer();
+	~Renderer();
+};
+
+#endif /* SDL_UTILS_RENDERER_H_ */
