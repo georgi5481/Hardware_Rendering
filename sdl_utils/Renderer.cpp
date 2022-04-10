50af136a5316964ecb96bf321fd8e123d17e1791
Georgi <georgi5481@abv.bg>
2022-04-10 19:30:32 +0300

    implementing render class

--- sdl_utils/Renderer.cpp
+++ sdl_utils/Renderer.cpp
@@ -4,6 +4,7 @@
 //C system includes
 
 //C++ system includes
+#include<iostream>
 
 //3rd-party includes
 #include <SDL_render.h>
@@ -11,10 +12,9 @@
 //Own includes
 
 
-/*SDL_CreateRenderer()  //pass SDL_RENDERER_ACCELERATED as flags
-SDL_SetRenderDrawColor()
-SDL_DestroyRenderer()
-SDL_RendererClear()
+/*
+
+
 SDL_RenderCopy()
 SDL_RenderPresent()*/
 
@@ -24,18 +24,39 @@ SDL_RenderPresent()*/
 int32_t Renderer::init(SDL_Window * window){
 	/*giving the SDL_CreateRenderer function:
 	 * 1 the window(pointer) where rendering is displayed,
-	 * 2 setting -1 for using default driver( DiretX in this case)
+	 * 2 setting -1 for using default driver(DiretX in this case)
 	 * 3 A flag for using the hardware acceleration instead of CPU.*/
-	SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	constexpr auto unspecifiedDriverId = -1;
+	_sdlRenderer = SDL_CreateRenderer(window, unspecifiedDriverId, SDL_RENDERER_ACCELERATED);
+	if(_sdlRenderer == nullptr)
+	{
+		std::cerr << "SDL_CreateRenderer failed. Reason:" << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	//SDL_SetRenderDrawColor will place a color on every pixel on the buffer we are clearing
+	if(EXIT_SUCCESS != SDL_SetRenderDrawColor(_sdlRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE)){
+		std::cerr << "SDL_SetRenderDrawColor failed. Reason:" << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+
+	}
 
 	return EXIT_SUCCESS;
 }
 
 void Renderer::deinit(){
-
+	if(_sdlRenderer){
+	SDL_DestroyRenderer(_sdlRenderer);
+	_sdlRenderer = nullptr;
+	}
 }
 
+
 void Renderer::clearScreen(){
+	if(EXIT_SUCCESS != SDL_RenderClear(_sdlRenderer)){
+		std::cerr << "SDL_RenderClear() failed. Reason:" << SDL_GetError() << std::endl;
+	}
 
 }
 
